1927ce56285a0ff0ab4b608ae24a1f94557ec96e
Arjun Narayanan <arjun14081999@gmail.com>
2025-08-17 11:53:06 -0500

    add step count feature

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pedometer/main.dart';
+import 'package:pedometer_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
